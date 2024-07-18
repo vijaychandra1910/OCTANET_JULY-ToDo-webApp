7487ac0a4a5c56bf97755cd7b62863214c4d5b18
VIJAY KUMAR CHANDRA <vijaychandra356@gmail.com>
2024-07-18 22:27:26 +0530

    todo

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value=== ''){
+        alert('Please enter the task!');
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li); //display li under the listContainer
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";  //after added the task
+    saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
